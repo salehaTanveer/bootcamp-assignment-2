d24dec5b82b2434c47d7b5bc6390ced6db5543eb
salehaTanveer <56721766+salehaTanveer@users.noreply.github.com>
2020-06-20 17:21:52 +0500

    Using print messages
    import print messages file and use Print_Message function
    
    included export default Print_Message in print_message.js

--- src/print_message.js
+++ src/print_message.js
@@ -3,7 +3,8 @@ import React from "react";
 function Print_Message(props){
     return(
         <div>
-            <h1> The message is <br/> {props.message} </h1>
+            <h1> The message is " {props.message} "</h1>
         </div>
     )
 }
+export default Print_Message;
